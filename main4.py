2e57ebf9f284c35c9542216c7576082a595feb57
Alexey Bolotov <stealthfear@gmail.com>
2019-03-20 11:29:45 +0500

    Update main4.py

--- main4.py
+++ main4.py
@@ -5,7 +5,7 @@ import pygame
 #GPIO.setwarnings(False)
 #GPIO.setmode(GPIO.BOARD)
 #GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-pygame.mixer.init()
+#pygame.mixer.init()
 #pygame.mixer.music.load("sound.wav")
 #pygame.mixer.music.play()
 
@@ -18,7 +18,7 @@ def task():
 def check_state(pin, label):
 	state = GPIO.input(pin)
 	if state == 0:
-		label.config(text='ВХОДИТЕ', bg='green'); pygame.mixer.music.load("sound.wav"); pygame.mixer.music.play()
+		label.config(text='ВХОДИТЕ', bg='green')#; pygame.mixer.music.load("sound.wav"); pygame.mixer.music.play()
 	elif state == 1:
 		label.config(text='ОЖИДАЙТЕ', bg='#FF0000')
 win = Tk()
@@ -27,7 +27,7 @@ win.title("Терминал вызова")
 win.geometry('1280x720')
 win.configure(background='black')
 
-l1 = Label(width=15, height=2, bg='#3515B0', text="ОКНО - 1", fg='white', font='Arial 80 bold')
+l1 = Label(width=15, height=2, bg='#3515B0', text="Проверка", fg='white', font='Arial 80 bold')
 l1.pack(side=TOP)
 
 
